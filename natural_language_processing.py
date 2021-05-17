f5d0f0b7f34396554a766359ef6f7cc05b871cae
spdpbn <shubhspd22@gmail.com>
2021-05-17 13:02:37 +0530

    Update natural_language_processing.py

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -37,9 +37,9 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# Training the Naive Bayes model on the Training set
-from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
+# Training the SVM model on the Training set
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'linear', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -50,4 +50,13 @@ print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
-accuracy_score(y_test, y_pred)
+print(accuracy_score(y_test, y_pred))
+
+
+
+
+
+
+
+
+
